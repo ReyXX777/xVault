bf8f72e6ac7511fd6db5859f631b13cd8197b191
Abhishek Kumar Azad <reyanshroyabhi@gmail.com>
2025-06-01 10:16:54 +0530

    yya

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  // Add any prod environment variables here
+  apiUrl: 'https://your-production-api.com/api',  // example
+};
---
+++ src/environments/environments.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  // Add any dev environment variables here
+  apiUrl: 'http://localhost:4200/api',  // example
+};
